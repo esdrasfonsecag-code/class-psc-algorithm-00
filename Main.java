15eabe48613193755774339392b0624f0d21fccf
Esdras <esdrasfonsecag@gmail.com>
2025-09-16 19:56:52 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+  public static void main(String[] args) {
+   String salaDesejada = "A219";
+   String salaInformada = "";
+   Scanner scanner = new Scanner(System.in)
+   
+   System.out.println(x:"Digite o número na tela: ");
+   salaInformada = scanner.next();
+   
+   if (salaDesejada.equals.salaInformada) {
+       System.out.println(x:"Você encontrou sua sala!")
+   } else {
+       System.out.println(x:"Você está perdido!\nSe encontre!")
+       }
+   scanner ;close();
+  }
+}
